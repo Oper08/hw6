e02d77ce86a0382ff068d4f3e7a84cbbf8e8ca0d
oper <seisenbaianuarr@gmail.com>
2024-11-06 12:40:42 +0500

    commit

---
+++ index.js
@@ -0,0 +1,12 @@
+
+let name = "Али";
+let age = 25;
+let isStudent = true;
+
+console.log("Аты: " + name + " | Типі: " + typeof name);
+console.log("Жасы: " + age + " | Типі: " + typeof age);
+console.log("Студент пе?: " + isStudent + " | Типі: " + typeof isStudent);
+
+
+age = age.toString();
+console.log("Жасы (жол ретінде): " + age + " | Типі: " + typeof age);
