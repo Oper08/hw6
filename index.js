ccdf385db7a69d04b92c999a8d2d1c006eedc86c
Ануар <seisenbaianuarr@gmail.com>
2025-01-15 08:48:30 +0500

    Hw Kimbat.js

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
 
-let name = "Али";
-let age = 25;
-let isStudent = true;
+// серверден деректер алу 
+async function fetchUsers() {
+  try {
+      const response = await fetch('https://jsonplaceholder.typicode.com/users');
+      if (!response.ok) {
+          throw new Error('Қате орын алды!');
+      }
+      const users = await response.json();
+      Userss(users);
+  } catch (error) {
+      console.error('Қате орын алды!', error.message);
+  }
+}
 
-console.log("Аты: " + name + " | Типі: " + typeof name);
-console.log("Жасы: " + age + " | Типі: " + typeof age);
-console.log("Студент пе?: " + isStudent + " | Типі: " + typeof isStudent);
+// хтмл
+function Userss(users) {
+  const container = document.getElementById('user-container');
+  users.forEach(user => {
+      const userBlock = document.createElement('div');
+      userBlock.className = 'user-block';
+      userBlock.innerHTML = `
+          <p>Қолданушы аты: <strong>${user.name}</strong></p>
+          <p>Email: <strong>${user.email}</strong></p>
+      `;
+      container.appendChild(userBlock);
+  });
+}
 
-
-age = age.toString();
-console.log("Жасы (жол ретінде): " + age + " | Типі: " + typeof age);
+// айпи косу
+fetchUsers();
